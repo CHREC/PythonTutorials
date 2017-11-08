8d4f7535895ff0a497bf0254755f4948b37795c0
Sid Bala <sidbala2310@gmail.com>
2017-11-07 23:20:47 -0500

    Add basicSyntax File

---
+++ basic_syntax.py
@@ -0,0 +1,40 @@
+# Print
+
+print ('Hello, Python!')  # displays characters within the quotes
+
+# Multi-Line Statements
+i = 1
+j = 2
+k = 3
+total = i +\
+        j +\
+        k
+# '\' character indicates that the code following in the next line is still part of the line above
+# brackets do not need '/' to continue on next Line
+
+# Quotes
+word = 'word'
+sentence = "sentence"
+paragraph = '"paragraph"'
+# any of the above quotation marks can be used to indicate strings as long the start and then end quotes are the Statements
+
+# Comments
+# '#' character indicates that characters following will not be executed when running the program
+
+# Waiting for User
+raw_input("\n\nPress the enter key to exit.")
+# when executed, the code pauses and waits for the user input to continue
+# '\n' indicates that the a blank line will be displayed and will move to the next line
+
+# Multiple Statement Groups
+import sys; x = 'foo'; sys.stdout.write(x+'\n')
+# ';' code typically written in the next line can be continued in the same line
+
+# Suites
+if x == 0:
+    suite
+elif x == 1:
+    suite
+else:
+    suite
+# Lines 35 to 36 represents a suite.
